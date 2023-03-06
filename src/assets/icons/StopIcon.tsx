3e090219d0a2153eac6c74995089fb0b1900433b
stark-eth <1319114370@qq.com>
2023-03-06 18:37:44 +0800

    change button

--- src/assets/icons/StopIcon.tsx
+++ src/assets/icons/StopIcon.tsx
@@ -1,4 +1,4 @@
 
-const StopIcon = () => (<svg viewBox="0 0 24 24" width="100" height="100"> <path d="M6 6h12v12H6z" fill="#ff0000" /> </svg>);
+const StopIcon = () => (<svg viewBox="0 0 24 24" width="50" height="50"> <path d="M6 6h12v12H6z" fill="#ff0000" /> </svg>);
 
 export default StopIcon;
