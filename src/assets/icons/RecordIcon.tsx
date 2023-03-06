da5aeaffba0559f1f3d391239f048e103ba548fc
stark-eth <1319114370@qq.com>
2023-03-06 18:33:06 +0800

    add color

--- src/assets/icons/RecordIcon.tsx
+++ src/assets/icons/RecordIcon.tsx
@@ -1,7 +1,4 @@
-const RecordIcon = () => (
-    <svg viewBox="0 0 24 24" width="24" height="24">
-        <path d="M12 17a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7zm0-2a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM12 4a4 4 0 0 1 4 4v4a4 4 0 1 1-8 0V8a4 4 0 0 1 4-4zm0-2a6 6 0 0 0-6 6v4a6 6 0 0 0 12 0V8a6 6 0 0 0-6-6z" />
-    </svg>
-);
+
+const RecordIcon = () => (<svg viewBox="0 0 24 24" width="100" height="100"> <path d="M12 17a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7zm0-2a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM12 4a4 4 0 0 1 4 4v4a4 4 0 1 1-8 0V8a4 4 0 0 1 4-4zm0-2a6 6 0 0 0-6 6v4a6 6 0 0 0 12 0V8a6 6 0 0 0-6-6z" fill="#007bff" /> </svg>);
 
 export default RecordIcon;
--- src/assets/icons/StopIcon.tsx
+++ src/assets/icons/StopIcon.tsx
@@ -1,7 +1,4 @@
-const StopIcon = () => (
-    <svg viewBox="0 0 24 24" width="24" height="24">
-        <path d="M6 6h12v12H6z" />
-    </svg>
-);
+
+const StopIcon = () => (<svg viewBox="0 0 24 24" width="100" height="100"> <path d="M6 6h12v12H6z" fill="#ff0000" /> </svg>);
 
 export default StopIcon;
